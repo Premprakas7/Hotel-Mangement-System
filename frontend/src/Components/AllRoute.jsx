6d85e28429ffbf999fcbd058009379a4d0068ae6
Premprakas7 <97504560+Premprakas7@users.noreply.github.com>
2023-07-17 10:16:34 +0530

    Req Auth added

--- frontend/src/Components/AllRoute.jsx
+++ frontend/src/Components/AllRoute.jsx
@@ -7,14 +7,15 @@ import AdminEdit from './Admin/AdminEdit'
 import SignUp from './SignUp/SignUp'
 import Login from './Login/Login'
 import Details from './User/Details'
+import ReqAuth from './ReqAuth'
 
 const AllRoute = () => {
   return (
     <div>
         <Routes>
             <Route path='/' element={<Home/>}/>
-            <Route path='/user' element={<User/>}/>
-            <Route path='/admin' element={<Admin/>}/>
+            <Route path='/user' element={<ReqAuth><User/></ReqAuth>}/>
+            <Route path='/admin' element={<ReqAuth><Admin/></ReqAuth>}/>
             <Route path='/admin/edit/:id' element={<AdminEdit/>}/>
             <Route path='/signup' element={<SignUp/>}/>
             <Route path='/login' element={<Login/>}/>
---
+++ frontend/src/Components/ReqAuth.jsx
@@ -0,0 +1,13 @@
+import { useSelector } from 'react-redux'
+import { Navigate, useLocation } from 'react-router-dom';
+
+const ReqAuth = ({children}) => {
+    const location=useLocation();
+    const auth=useSelector(state=>state.AuthReducer.isAuth);
+    if(!auth){
+        return <Navigate to='/login' state={{from:location }} replace />
+    }
+  return children
+};
+
+export default ReqAuth;
