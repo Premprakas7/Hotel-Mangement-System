2b6f0ab6d63ef2ec1ee3fb48b42f886c61abfdc8
Premprakas7 <97504560+Premprakas7@users.noreply.github.com>
2023-09-23 16:18:58 +0530

    update action.js

--- frontend/src/Redux/auth/action.js
+++ frontend/src/Redux/auth/action.js
@@ -1,19 +1,6 @@
 import axios from "axios";
 import * as Types from "./actionTypes";
 
-export const register = (payload) => (dispatch) => {
-  dispatch({ type: Types.REGISTER_REQUEST });
-  return axios
-    .post("https://reqres.in//api/register", payload)
-    .then((r) => {
-      dispatch({ type: Types.REGISTER_SUCCESS, payload: r.data });
-      return Types.REGISTER_SUCCESS;
-    })
-    .catch((e) => {
-      dispatch({ type: Types.REGISTER_FAILURE, payload: e });
-      return Types.REGISTER_FAILURE;
-    });
-};
 
 export const login = (params) => (dispatch) => {
   dispatch({ type: Types.LOGIN_REQUEST });
