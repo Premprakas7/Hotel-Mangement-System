17855cb23248eed965492b6a14d6647e546393b3
Premprakas7 <97504560+Premprakas7@users.noreply.github.com>
2023-09-26 18:24:13 +0530

    port changed

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,10 +1,10 @@
 const app = require("./index");
 const connect=require("./configs/db");
 
-app.listen(5000, async function () {
+app.listen(5001, async function () {
   try {
     await connect();
-    console.log("listening on port 5000");
+    console.log("listening on port 5001");
   } catch (err) {
     console.error(err.message);
   }
