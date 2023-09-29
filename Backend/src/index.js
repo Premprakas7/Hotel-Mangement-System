0cba0ffa2c69ee5bdfd66c4f3c420b93a0014144
Premprakas7 <97504560+Premprakas7@users.noreply.github.com>
2023-09-29 17:52:08 +0530

    updated

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,5 +1,8 @@
 const express=require("express");
 const userController=require("./controllers/user.controllers")
+const authController=require("./controllers/auth.controllers")
+const registerController=require("./controllers/register.controller")
+const loginController=require("./controllers/login.controller")
 const cors=require("cors")
 const mongoose=require("mongoose");
 mongoose.set('strictQuery', true);
@@ -7,6 +10,9 @@ const app=express()
 app.use(express.json());
 app.use(cors())
 
-app.use("/users", userController)
+app.use("/users", userController);
+app.use("auth", authController);
+app.use("register",registerController);
+app.use("login", loginController);
 
 module.exports=app;
