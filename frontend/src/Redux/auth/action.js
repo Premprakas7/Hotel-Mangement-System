5cd9581499dd1f5136fd9e72392fba745e02d01b
Premprakas7 <97504560+Premprakas7@users.noreply.github.com>
2024-01-18 18:51:45 +0530

    updated

--- frontend/src/Redux/auth/action.js
+++ frontend/src/Redux/auth/action.js
@@ -3,7 +3,7 @@ import { LOGIN_FAILURE, LOGIN_REQUEST, LOGIN_SUCCESS } from "./actionTypes"
 
 export const login=(payload)=>(dispatch)=>{
     dispatch({type:LOGIN_REQUEST})
-   return axios.post("https://hotel-backend-3tcb.onrender.com/login")
+   return axios.post("http://localhost:5001/login", payload)
     .then((res)=>{ return dispatch({type:LOGIN_SUCCESS, payload:res.data.token})})
     .catch((err)=>{return dispatch({type:LOGIN_FAILURE })})
     }
